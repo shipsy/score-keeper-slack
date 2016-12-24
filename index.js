222fe6123c4bf118526d64e1312c16a11d0b0f74
Himanshu Gupta <himgup321@gmail.com>
2016-12-24 20:35:59 +0530

    Add command to get latest transactions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var request = require('request');
 var MongoClient = require('mongodb').MongoClient;
 var https = require('https');
 var http = require('http');
+var moment = require('moment');
 
 var server = null;
 var usessl = false;
@@ -58,17 +59,6 @@ var doAddition = function(splitted, requestBody) {
 			$inc: {
 				['scores.' + thingName]: change
 			},
-			// $push: {
-			// 	history: {
-			// 		createdAt: new Date(),
-			// 		createdById: requestBody.user_id,
-			// 		createdByName: requestBody.user_name,
-			// 		channelId: requestBody.channel_id,
-			// 		channelName: requestBody.channel_name,
-			// 		thingName: thingName,
-			// 		change: change
-			// 	}
-			// },
 			$setOnInsert: { 
 				createdAt: currentTime
 			}
@@ -89,8 +79,43 @@ var doAddition = function(splitted, requestBody) {
 	});
 };
 
+function getTransactions(splitted, requestBody, req, res) {
+	var groupName = splitted[1];
+	if (!groupName)
+		return Promise.reject(createError('Group name should be present'));
+	groupName = groupName.toLowerCase();
 
-function getScores(splitted, requestBody) {
+	return app.db.collection('scoreHistory').find({groupName: groupName})
+		.sort({createdAt: -1})
+		.limit(20)
+		.toArray()
+	.then(function(transactions) {
+		var textToSend = '';
+		if (transactions.length === 0) {
+			textToSend = 'No transactions found';
+			return res.end(textToSend);
+		}
+
+		textToSend = 'LATEST TRANSACTIONS:\n'
+		textToSend += transactions
+		.map(function(elem) {
+			var toRet = elem.change.toString() + ' for ' + elem.thingName + ' by ' + 
+				elem.createdByName + ' on ';
+			var createdAtString = 'unknown';
+			if (elem.createdAt) {
+				createdAtString = moment(elem.createdAt).format('DD-MMM-YY HH:mm');
+			}
+			toRet += createdAtString;
+			return toRet;
+		})
+		.join('\n');
+
+		res.end(textToSend);
+	});
+};
+
+
+function getScores(splitted, requestBody, req, res) {
 	var groupName = splitted[1];
 	if (!groupName)
 		return Promise.reject(createError('Group name should be present'));
@@ -172,6 +197,10 @@ app.post('/', function(req, res) {
 			});
 		}
 
+		if (commandType === 'transactions' || commandType === 'transaction') {
+			return getTransactions(splitted, requestBody, req, res);
+		}
+
 		return Promise.reject(createError('Command not supported'));
 	})
 	.catch(errorHandler);
@@ -185,86 +214,6 @@ function createError(message, options) {
 		toRet[key] = options[key];
 	return toRet;
 }
-
-// function getUsageHelp(commandName) {
-// 	function createSample(target) {
-// 		return commandName + ' *' + target + '* I know what you did last summer';
-// 	}
-
-// 	var text = 'Expected usage: \n' +
-// 		commandName + ' help -- Displays help message.\n' +
-// 		createSample('@user') + ' -- Sends to the specified user.\n' +
-// 		createSample('#channel') + ' -- Sends to the specified public channel.\n' +
-// 		createSample('group') + ' -- Sends to the specified private group.\n' +
-// 		createSample(':here') + ' -- Sends to the current group/channel/DM where you type this command.';
-
-// 	return text;
-// }
-
-// function getFullHelp(commandName) {
-// 	var text =
-// 		'Allows to send anonymous messages to users, channels and groups.\n' +
-// 		'The most convenient and safe way is to open up a conversation with slackbot in Slack and type the commands there, so that nobody detects that you are typing and you don\'t accidentally reveal yourself by typing an invalid command.\n' +
-// 		'Messages and authors are not stored, and the sources are available at <https://github.com/TargetProcess/slack-anonymous>.\n' +
-// 		'\n' +
-// 		getUsageHelp(commandName);
-
-// 	return text;
-// }
-
-// function createResponsePayload(requestBody) {
-// 	if (!requestBody) {
-// 		return createError('Request is empty');
-// 	}
-
-// 	var text = requestBody.text;
-// 	var command = requestBody.command;
-
-// 	if (!text || text === 'help') {
-// 		return createError(getFullHelp(command));
-// 	}
-
-// 	var splitted = text.split(" ");
-// 	if (splitted.length <= 1) {
-// 		return createError(getUsageHelp(command));
-// 	}
-
-// 	var target = splitted[0];
-// 	var remainingText = splitted.slice(1).join(' ');
-// 	remainingText = 'Someone said "' + remainingText + '"';
-
-// 	if (target === ':here') {
-// 		return {
-// 			channel: requestBody.channel_id,
-// 			text: remainingText
-// 		};
-// 	}
-
-// 	return {
-// 		channel: target,
-// 		text: remainingText
-// 	};
-// }
-
-// app.post('/', function(req, response) {
-// 	var payloadOption = createResponsePayload(req.body);
-// 	if (payloadOption.error) {
-// 		response.end(payloadOption.error);
-// 		return;
-// 	}
-// 	request({
-// 		url: process.env.POSTBACK_URL,
-// 		json: payloadOption,
-// 		method: 'POST'
-// 	}, function (error) {
-// 		if(error) {
-// 			response.end('Unable to post your anonymous message: ' + JSON.stringify(error));
-// 		} else {
-// 			response.end('Delivered! :cop:');
-// 		}
-
-// 	});
-// });
 
 app.get('/', function(request, response) {
 	response.write('HELLO THERE');
